60d82ba283311ba38950ca5f5c03039070d00741
priyanshu-bhatt <priyanshubhatt1892001@gmail.com>
2023-02-03 23:15:08 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,230 @@
-import logo from './logo.svg';
+// import React, { useState } from 'react'
+
+// export default function App() {
+//   const [name,setname]=useState("")
+//   const [email,setmail]=useState("")
+//   const [gender,setgender]=useState("")
+//   const [phone,setphone]=useState("")
+//   const [classNAME,setclassNAME]=useState("")
+//   const [emptyerror,setemptyerror]=useState("")
+//   const [nameerror,setnameerror]=useState("")
+//   const [emailerror,setemailerror]=useState("")
+//   const [gendererror,setgendererror]=useState("select any")
+//   const [phoneerror,setphoneerror]=useState("")
+//   const [classNAMEerror,setclassNAMEerror]=useState("")
+
+//   const validate=()=>{
+//     if(name === " " || email==="" || classNAME ==="" || phone===""|| gender==="")
+//     {
+//       setemptyerror("All fields are mandatory")
+//       return false
+//     }
+//     if(!name.match(/^[A-Za-z0-9]/)){
+      
+//       setnameerror("Name error")
+//       return false
+//     }
+//     if(!email.includes("@")){
+//       setemailerror("must contain @")
+//       return false
+//     }
+//     if(!gender.match(/male|other/i)){
+//       setgendererror("select correct gender")
+//       return false
+//     }
+//     if(!phone.length===10){
+//       setphoneerror("10 digit phone required")
+//       return false
+//     }
+//   }
+//   const handlesubmit=(e)=>{
+//     e.preventDefault()
+//     const isValid =validate()
+//   }
+//   return (
+//     <div>
+//       <div className='App'>
+//         <form onSubmit={handlesubmit}>
+//           <input type="text" placeholder='name' value={name}
+//            onChange={(e)=>{setname(e.target.value)}}/>
+//            {nameerror}
+//           <input type="text" placeholder='email'value={email}
+//            onChange={(e)=>{setmail(e.target.value)}}/>
+//            {emailerror}
+//           <select name='gender' value={gender} onChange={(e)=>{setgender(e.target.value)}}>
+          
+//           <option  value="select any" >Select Any</option>
+//             <option  value="male">male</option>
+//             <option value="female">female</option>
+//             <option value="other">other</option>
+            
+//           </select>
+//           {gendererror}
+//           <input type="text" value={phone} placeholder='phone number'
+//           onChange={(e)=>{setphone(e.target.value)}}/>
+//           {phoneerror}
+//           <input type="classNAME"  value={classNAME} placeholder='classNAME'
+//           onChange={(e)=>{setclassNAME(e.target.value)}}/>
+          
+//           <input type="submit" value="Submit" />
+//           {emptyerror}
+//         </form>
+//       </div>
+//     </div>
+//   )
+// }
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name,setName] = useState("")
+  const [mother,setMotherName] = useState("")
+  const [father,setFatherName] = useState("")
+  const [address,setAddress]=useState("")
+  const [email,setEmail] = useState("")
+  const [gender,setGender] = useState("Select any")
+  const [phoneNumber,setPhoneNumber] = useState("")
+  const [classNAME,setclassNAME] = useState("")
+  const [emptyError,setEmptyError] = useState("")
+  const [nameError,setNameError] = useState("")
+  const [emailError,setEmailError] = useState("")
+  const [genderError, setGenderError] = useState("")
+  const [phoneNumberError,setPhoneNumberError] = useState("")
+  const [classNAMEError, setclassNAMEError] = useState("")
+  const [message,setmessage] = useState("")
+  const [mothernameError,setMotherNameError] = useState("")
+  const [fathernameError,setFatherNameError] = useState("")
+  const [addressError,setAddressError]=useState("")
+const validate=()=>{
+  if(
+    name === "" || email === "" || gender==="" || phoneNumber === "" ||classNAME==="" || mother ==="" || father ===""  || address===""
+  ){
+    setEmptyError("All fields are mandatory")
+    setmessage("")
+    return false 
+  }
+  if(!name.match(/^[A-Za-z- ]+$/)){
+    setNameError("Name is not alphanumeric")
+    setmessage("")
+    return false;
+  }
+  if(!mother.match(/^[A-Za-z- ]+$/)){
+    setMotherNameError("Name is not alphanumeric")
+    setmessage("")
+    return false;
+  }
+  if(!father.match(/^[A-Za-z- ]+$/)){
+    setFatherNameError("Name is not alphanumeric")
+    setmessage("")
+    return false;
+  }
+  
+  if(!email.includes("@")){
+  setEmailError("must contain @")
+  setmessage("")
+  return false;
+  }
+  if(!gender.match(/male|female|other/i)){
+     setGenderError("Invaid Entry")
+     setmessage("")
+     return false
+  }
+  if(!phoneNumber.match(/^[0-9]+$/)){
+     setPhoneNumberError("Must contain only numbers")
+     setmessage("")
+     return false;
+  }
+  if(!classNAME.match(/^(?:[1-9]|0[1-9]|12)$/)){
+    setclassNAMEError("Invalid")
+    setmessage("")
+    return false
+  }
+  return true
+}
+const reseterrordefault=()=>{
+  setEmptyError("")
+  setEmailError("")
+  setGenderError("")
+  setNameError("")
+  setclassNAMEError("")
+  setPhoneNumberError("")
+  setFatherNameError("")
+  setMotherNameError("")
+  setAddressError("")
+}
+
+const handleSubmit=(e)=>{
+  e.preventDefault()
+  reseterrordefault()
+  const isValid = validate()
+  if(isValid){
+    setName("")
+    setEmail("")
+    setGender("select any")
+    setPhoneNumber("")
+    setclassNAME("")
+    setMotherName("")
+    setFatherName("")
+    reseterrordefault()
+    setmessage(name)
+    setAddress("")
+  }
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Admission Form</h1>
+    <form onSubmit={handleSubmit}>
+  
+    <label> Mother's Name</label>
+      <input type="text" placeholder='Mother Name' value={mother} 
+      onChange={(e)=>{setMotherName(e.target.value)}}/><br/>
+      <div id="error">{mothernameError}</div><br/>
+
+      <label>Father's Name</label>
+      <input type="text" placeholder='Father Name' value={father} 
+      onChange={(e)=>{setFatherName(e.target.value)}}/><br/>
+      <div id="error">{fathernameError}</div><br/>
+
+      <label>Student's Name</label>
+      <input type="text" placeholder='Student Name' value={name} 
+      onChange={(e)=>{setName(e.target.value)}}/><br/>
+      <div id="error">{nameError}</div><br/>
+
+      <label>Email</label>
+      <input type="text" placeholder='email' value={email}
+      onChange={(e)=>{setEmail(e.target.value)}}/><br/><br/>
+      <div id="error">{emailError}</div><br/>
+
+      <label> Address</label>
+     <textarea rows="1" value={address} placeholder='enter address' onChange={(e)=>{setAddress(e.target.value)}}/><br/>
+      <div id="error">{addressError}</div><br/>
+
+      <label>Class Applying For</label> 
+      <input type="text" value={classNAME} placeholder='Enter class'
+      onChange={(e)=>{setclassNAME(e.target.value)}}/><br/>
+      <div id="error">{classNAMEError}</div><br/>
+
+      <label>Gender</label> 
+      <select name="gender" value={gender} onChange={(e)=>{setGender(e.target.value)}}>
+        <option value="Select Any" selected>Select Any</option>
+        <option value="male">male</option>
+        <option value="female">female</option>
+        <option value="other">other</option>
+      </select><br/>
+      <div id="error">{genderError}</div><br/>
+      <label>Phone Number</label> 
+      <input type="text" value={phoneNumber} placeholder='Phone Number'
+      onChange={(e)=>{setPhoneNumber(e.target.value)}} /><br/>
+      <div id="error">{phoneNumberError}</div><br/>
+      
+      <div id="error">{emptyError}</div><br/>
+      <input id="bt" className=" btn btn-success" type="submit" value="Submit"/><br/>
+      
+    </form>
+    <div>
+      <h2 >{message?message+" registered":""}</h2>
+    </div>
     </div>
   );
 }
